e096217f9162daa1d5939548b486af034dbd7c17
MintNiphada <niphada.y@kkumail.com>
2026-01-21 20:30:58 +0700

    react setup

---
+++ node_modules/.pnpm/node_modules/ionic-app-base/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'moodmood',
+  webDir: 'dist'
+};
+
+export default config;
